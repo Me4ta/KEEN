c8dba6dd331cde73dcc6b731e053c87dc1b38cc1
Helen Akimova <me4ta18@gmail.com>
2014-03-13 14:58:15 -0700

    Made a logout

--- Registration_page/registration_page_test.py
+++ Registration_page/registration_page_test.py
@@ -10,8 +10,8 @@ driver.get("http://mb.test.ingenio.com/index.html#/register")
 # input e-mail, password, confirm password
 wait = WebDriverWait(driver, 10)
 email_field = wait.until(EC.element_to_be_clickable((By.ID, "regEmail")))
-
-email_field.send_keys("test03@example.com")
+test_user_email = "test04@example.com"
+email_field.send_keys(test_user_email)
 
 password_field = driver.find_element_by_id("regPassword")
 password_field.send_keys("123456")
@@ -22,11 +22,18 @@ confirm_password_field.send_keys("123456")
 register_button = driver.find_element_by_id("btnRegister")
 register_button.click()
 
-
-
 # save the created name
+common_menu = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "show-menu-button")))
+common_menu.click()
+
 # make a logout
+logout_button = wait.until(EC.element_to_be_clickable((By.ID, "logout")))
+logout_button.click()
+
 # login with created name and password
+common_menu = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "show-menu-button")))
+common_menu.click()
+
 # assert login
 # logout
 #deiver.quit()
